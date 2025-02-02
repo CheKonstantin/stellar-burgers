bc279967606d5b8a0d856d41f67e6eb812e7cba0
Konstantin Chepurin <chepurin.konst@yandex.ru>
2025-02-02 19:58:34 +0700

    add int tests

--- cypress/e2e/constructorTest/constructor.cy.ts
+++ cypress/e2e/constructorTest/constructor.cy.ts
@@ -1,42 +1,19 @@
-const mockUrl = 'http://localhost:3002/';
-const mockBun = `[data-cy="643d69a5c3f7b9001cfa093c"]`;
-const mockSauce = `[data-cy="643d69a5c3f7b9001cfa0942"]`;
-const mockMain = `[data-cy="643d69a5c3f7b9001cfa0941"]`;
-
-const refCard = '[data-cy=cyCard]';
-const refModal = '[data-cy=cyModal]';
-const refCloseModal = '[data-cy=closeModal]';
+import { mockBun, getIngr, mockSauce, mockMain } from '../constants';
 
 beforeEach(() => {
-  cy.intercept('GET', '/api/ingredients', {
-    fixture: 'mockIngredients.json'
-  }).as('getIngredients');
-
-  cy.visit(mockUrl);
+  getIngr();
 });
 
 describe('Constructor test', () => {
-  it('get ingredients', function () {
-    cy.wait('@getIngredients');
+  it('get ingredients', () => {
+    cy.wait('@ingredients');
   });
-  it('add ingr in constructor', async () => {
-    const btnBun = cy.get(mockBun).contains('Добавить');
-    btnBun.click();
+  it('add ingr in constructor', () => {
+    cy.get(mockBun).contains('Добавить').click();
     cy.get('[data-cy=cyBun]').should('exist');
-
-    const btnIngrSauce = cy.get(mockSauce).contains('Добавить');
-    btnIngrSauce.click();
+    cy.get(mockSauce).contains('Добавить').click();
     cy.get('[data-cy=cyMain]').should('exist');
-
-    const btnIngrMain = cy.get(mockMain).contains('Добавить');
-    btnIngrMain.click();
+    cy.get(mockMain).contains('Добавить').click();
     cy.get('[data-cy=cyMain]').should('exist');
-  });
-
-  describe('modal test', () => {
-    it('open modal', async () => {
-      cy.get(refCard).click();
-      cy.get(refModal).should('exist');
-    });
   });
 });
